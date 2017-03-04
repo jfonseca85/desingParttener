4a0110888135d9628ca33dc8b01a3e5d7cac2707
jfonseca <jfonseca@bcfsolutions.net.br>
2017-03-04 16:44:36 -0300

    Mudanca no COnstrutor do CalculaImposto

--- src/main/java/br/com/alura/strategy/CalculaIMposto.java
+++ src/main/java/br/com/alura/strategy/CalculaIMposto.java
@@ -1,16 +1,8 @@
 package br.com.alura.strategy;
 
 public class CalculaIMposto {
-	private Orcamento orcamento;
-	private Imposto imposto;
 
-	public CalculaIMposto(Orcamento orcamento, Imposto imposto){
-		this.orcamento = orcamento;
-		this.imposto = imposto;
-		
-	}
-	
-	public void calculaTaxa(){
-		System.out.println(this.imposto.calcula(orcamento));
+	public void calculaTaxa(Orcamento orcamento, Imposto imposto){
+		System.out.println(imposto.calcula(orcamento));
 	}
 }

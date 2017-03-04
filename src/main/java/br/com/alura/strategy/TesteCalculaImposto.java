4e5326358df99431225060ee586a718bcd5ad4b4
jfonseca <jfonseca@bcfsolutions.net.br>
2017-03-04 16:45:26 -0300

    Mudanca no teste  do CalculaImposto

--- src/main/java/br/com/alura/strategy/TesteCalculaImposto.java
+++ src/main/java/br/com/alura/strategy/TesteCalculaImposto.java
@@ -5,9 +5,12 @@ public class TesteCalculaImposto {
 	public static void main(String[] args) {
 		Orcamento orcamento = new Orcamento(500);
 		Imposto icms = new ICMS();
-		CalculaIMposto calculaIMposto = new CalculaIMposto(orcamento, icms);
-		calculaIMposto.calculaTaxa();
+		Imposto iss = new ISS();
+		CalculaIMposto calculaIMposto = new CalculaIMposto();
+		calculaIMposto.calculaTaxa(orcamento, icms);
+		calculaIMposto.calculaTaxa(orcamento, iss);
 
+		
 	}
 
 }
